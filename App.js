b4ceb3f359705ef3b926a60a257e4a96c2f2efa4
juancaste <juandiegocastellanos7@gmail.com>
2022-11-07 09:50:20 -0500

    letter added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>mira quien llego!</Text>
+      <Text>mira quien llegoo!</Text>
       <StatusBar style="auto" />
     </View>
   );
